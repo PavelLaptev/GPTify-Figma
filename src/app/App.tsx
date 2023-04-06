c4c5389ea1c27164fc4034fd451b20e4ad0f6369
Pavel Laptev <pawellaptew@gmail.com>
2023-04-06 02:39:54 +0200

    :construction: wip

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,22 @@ import styles from "./app.module.scss";
 
 import { TranslateSection } from "./sections/TranslateSection";
 
+const apiENVKey = process.env.REACT_APP_OPENAI_API_KEY;
+
 const App = () => {
+  const [inputValue, setInputValue] = React.useState(apiENVKey);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <section className={styles.wrap}>
       <h1>GPTify</h1>
-      <TranslateSection />
+      <input type="text" onChange={handleInput} value={inputValue} />
+      <button>Apply</button>
+
+      <TranslateSection apiKey={inputValue} />
     </section>
   );
 };
--- src/app/sections/TranslateSection/index.tsx
+++ src/app/sections/TranslateSection/index.tsx
@@ -1,47 +1,77 @@
 import React from "react";
 import { Configuration, OpenAIApi } from "openai";
 
-const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+// const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
-const configuration = new Configuration({
-  apiKey: apiKey,
-});
-const openai: OpenAIApi = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: apiKey,
+// });
+// const openai: OpenAIApi = new OpenAIApi(configuration);
+
+interface Props {
+  apiKey: string;
+}
 
 // Add parent class for sub-components
-export const TranslateSection: React.FC = () => {
-  const sendRequest = () => {
-    parent.postMessage({ pluginMessage: { type: "get-textnodes" } }, "*");
-  };
+export const TranslateSection: React.FC<Props> = (props) => {
+  const [apiKey, setApiKey] = React.useState(props.apiKey);
 
-  const handleTranslation = async (arr: textObject[]) => {
-    const prompt = `Translate text in this array for keys "text" into german ${JSON.stringify(
-      arr
-    )}`;
+  // const sendRequest = () => {
+  //   parent.postMessage({ pluginMessage: { type: "get-textnodes" } }, "*");
+  // };
+
+  // const handleTranslation = async (arr: textObject[]) => {
+  //   const prompt = `Translate text into german: "hello to all good people!"`;
+
+  //   const response = await openai.createCompletion({
+  //     model: "text-davinci-003",
+  //     prompt: prompt,
+  //   });
+
+  //   console.log(response.data.choices[0].text);
+  // };
+
+  // React.useEffect(() => {
+  //   window.onmessage = (event) => {
+  //     const msg = event.data.pluginMessage;
+
+  //     if (msg.type === "get-textnodes") {
+  //       const textObjects = msg.textObjects;
+
+  //       handleTranslation(textObjects);
+  //     }
+  //   };
+  // }, []);
+
+  const createRequest = async () => {
+    const configuration = new Configuration({
+      apiKey: apiKey,
+    });
+
+    const openai: OpenAIApi = new OpenAIApi(configuration);
+
+    const prompt = `Translate text into german: "hello to all good people!"`;
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
+      max_tokens: 100,
+      temperature: 0.9,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
 
     console.log(response.data.choices[0].text);
   };
 
   React.useEffect(() => {
-    window.onmessage = (event) => {
-      const msg = event.data.pluginMessage;
-
-      if (msg.type === "get-textnodes") {
-        const textObjects = msg.textObjects;
-
-        handleTranslation(textObjects);
-      }
-    };
-  }, []);
+    setApiKey(props.apiKey);
+  }, [props.apiKey]);
 
   return (
     <section>
-      <button onClick={sendRequest}>Translate selected</button>
+      <button onClick={createRequest}>Translate selected</button>
     </section>
   );
 };
